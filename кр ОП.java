389c6d9ec6aee809a968f518a34782cc67ab96b8
valeruyaaaa <157934191+valeruyaaaa@users.noreply.github.com>
2024-06-30 20:00:59 +0300

    Add files via upload

---
+++ кр ОП.java
@@ -0,0 +1,58 @@
+public class Matrix {
+    private final int rows;
+    private final int cols;
+    private final int[][] data;
+
+    public Matrix(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.data = new int[rows][cols];
+    }
+
+    public void set(int row, int col, int value) {
+        data[row][col] = value;
+    }
+
+    public Matrix add(Matrix other) {
+        Matrix result = new Matrix(rows, cols);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result.data[i][j] = this.data[i][j] + other.data[i][j];
+            }
+        }
+        return result;
+    }
+
+    public Matrix subtract(Matrix other) {
+        Matrix result = new Matrix(rows, cols);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result.data[i][j] = this.data[i][j] - other.data[i][j];
+            }
+        }
+        return result;
+    }
+
+    public Matrix multiply(Matrix other) {
+        Matrix result = new Matrix(rows, other.cols);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < other.cols; j++) {
+                for (int k = 0; k < cols; k++) {
+                    result.data[i][j] += this.data[i][k] * other.data[k][j];
+                }
+            }
+        }
+        return result;
+    }
+
+    public void print() {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(data[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
